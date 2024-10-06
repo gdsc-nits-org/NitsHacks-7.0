8aa76fa9859543e352d9db480679360cfde2633f
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-10-06 23:24:19 +0530

    fix

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import style from "./Home.module.scss";
-import { Footer } from "../../Components/index";
+import { Footer, Event, Navbar } from "../../Components/index";
 
 const Home = () => {
   return (
     <main className={style.home}>
+      <Navbar />
       <h1>Home Page</h1>
+      <Event />
       <Footer />
     </main>
   );
