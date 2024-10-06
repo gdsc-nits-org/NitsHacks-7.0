ed746b1553f5df6e3a456acad03ed156e8b44f36
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2024-10-06 10:18:48 +0530

    fixing some responsiveness issues

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -7,32 +7,34 @@ const Event = () => {
   };
 
   return (
-    <div className={styles.eventCont}>
+    <div className={styles.eventBox}>
       <h1 className={styles.heading}>Events</h1>
-      <div className={styles.pagebody}>
-        {events.map((event) => {
-          return (
-            <div
-              key={event.id}
-              className={styles.card}
-              onClick={() => handleClick(event.id)}
-            >
-              <div className={styles.innerCont}>
-                <img className={styles.img1} src={event.photo} alt={event.name} />
-                <div className={styles.content}>
-                  <div className={styles.rectangle}>
-                    <div className={styles.box}>
-                      <p className={styles.title}>{event.name}</p>
+      <div className={styles.eventCont}>
+        <div className={styles.pagebody}>
+          {events.map((event) => {
+            return (
+              <div
+                key={event.id}
+                className={styles.card}
+                onClick={() => handleClick(event.id)}
+              >
+                <div className={styles.innerCont}>
+                  <img className={styles.img1} src={event.photo} alt={event.name} />
+                  <div className={styles.content}>
+                    <div className={styles.rectangle}>
+                      <div className={styles.box}>
+                        <p className={styles.title}>{event.name}</p>
+                      </div>
                     </div>
-                  </div>
-                  <p className={styles.description2}>{event.desc2}</p>
+                    <p className={styles.description2}>{event.desc2}</p>
 
-                  <div className={styles.btn5}>View More!</div>
+                    <div className={styles.btn5}>View More!</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
