36b6615418d9f7be9d2e79e49f65d8cbd0cc4f83
GulistaKhatun06 <gulista_ug_23@civil.nits.ac.in>
2024-10-07 20:03:11 +0530

    my commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,11 @@
 import style from "./Home.module.scss";
 import { Navbar, Footer } from "../../Components/index";
-import { NitsHacks, Tecno } from "../../Components/About";
 
 const Home = () => {
   return (
     <main className={style.home}>
       <Navbar />
       <h1>Home Page</h1>
-      <NitsHacks />
-      <Tecno />
       <Footer />
     </main>
   );
