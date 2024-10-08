cebd49a104cbbedb8269ed2fba83dc0f53f3579e
barnilsarma <barnilsarma@gmail.com>
2024-10-08 23:32:24 +0530

    half work done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,28 +1,73 @@
 import styles from "./Hero.module.scss";
-import { useRef,useEffect } from "react";
 
-const Radar=()=>{
-  return(
+const Radar = () => {
+  return (
     <div className={styles.radar}>
-      
+      <div className={styles.redLinesCont}>
+      </div>
+      <div className={styles.yellowLinesCont}>
+        <div className={styles.rect1}>
+          <div className={styles.rect2}>
+            <div className={styles.rect3}>
+              <div className={styles.rect4}>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-const DirectionBtn=(props)=>{
-  return(
+const DirectionBtn = () => {
+  return (
     <button className={styles.DirectionBtn}>
-      <img className={styles.img} src="/images/dirbtn.webp"/>
+      <img className={styles.img} src="/images/dirbtn.webp" alt="direction button" />
     </button>
   );
 }
 
-const ControlStation=()=>{
-  return(
+const Bulb = (props) => {
+  if(props.bulb==="green"){
+    return (
+      <div className={styles.BulbCont}>
+        <div className={styles.greenBulb}>
+        </div>
+      </div>
+    );
+  } 
+  if(props.bulb==="red"){
+    return (
+      <div className={styles.BulbCont}>
+        <div className={styles.redBulb}>
+        </div>
+      </div>
+    );
+  }
+  if(props.bulb==="blue"){
+    return (
+      <div className={styles.BulbCont}>
+        <div className={styles.blueBulb}>
+        </div>
+      </div>
+    );
+  }
+  
+    return(
+      <div className={styles.BulbCont}>
+        <div className={styles.greenBulb}>
+        </div>
+      </div>
+    );
+   
+}
+
+const ControlStation = () => {
+  return (
     <div className={styles.ControlStation}>
       <div className={styles.upperRow}>
         <div className={styles.dirbtncont}>
-          <DirectionBtn button="left"/>
+          <DirectionBtn button="left" />
         </div>
         <div className={styles.radarCont}>
           <Radar />
@@ -41,23 +86,46 @@ const ControlStation=()=>{
 }
 
 const Hero = () => {
-  return( 
-    <>
-      <div className={styles.hero}>
+  return (
+    <div className={styles.hero}>
         <div className={styles.rocketFront}>
+          <div className={styles.watchBulbs}>
+            <div className={styles.upper}>
+              <Bulb bulb="blue" />
+              <Bulb bulb="blue" />
+            </div>
+            <div className={styles.lower}>
+              <Bulb blue="red"/>
+            </div>
+          </div>
           <div className={styles.windshield}>
 
           </div>
           <div className={styles.ControlStationCont}>
-            <ControlStation/>
+            <div className={styles.lights}>
+              <div className={styles.left}>
+                <Bulb bulb="green" />
+                <Bulb bulb="green" />
+              </div>
+              <div className={styles.right}>
+                <div className={styles.rightChild}>
+                  <div className={styles.grandchild1}><Bulb bulb="red" /></div>
+                  <div className={styles.grandchild2}>
+                    <Bulb bulb="green" />
+                    <Bulb bulb="green" />
+                    <Bulb bulb="green" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <ControlStation />
           </div>
           <div className={styles.rocket}>
-            <img src="/images/rocket-cockpit.png" className={styles.cockpit}/>
+            <img src="/images/rocket-cockpit.png" className={styles.cockpit} alt="cockpit" />
           </div>
         </div>
       </div>
-    </>
-    
+
   );
 };
 
