ea77cb98deca3f49ee73fe5e41bdabfd62da19dc
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-10-08 16:19:19 +0530

    preview add

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-
-import { Home, Error, Event, Faq, Team } from "./Pages";
-import AboutNits from "./Components/About/NitsHacks/NitsHacks";
-import AboutTech from "./Components/About/Tecno/Tecno";
+import { useState } from "react";
+import { Home } from "./Pages";
+import { Preview } from "./Components";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/event/:id" element={<Event />} />
-      <Route path="/faq" element={<Faq />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="*" element={<Error />} />
-      <Route path="/AboutNits" element={<AboutNits />} />
-      <Route path="/AboutTech" element={<AboutTech />} />
-    </Routes>
-  );
+  const [loaded, setLoaded] = useState(false);
+  return loaded ? <Home /> : <Preview setLoaded={setLoaded} />;
 };
 
 export default App;
--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
   return (
     <div className={styles.error}>
       <img
-        src="https://res.cloudinary.com/dmhbmurzw/image/upload/v1728275605/bcc3e1d91add532aefb040cb0356c2db_rrj1ow.png"
+        src="https://res.cloudinary.com/dmhbmurzw/image/upload/v1728275605/bcc3e1d91add532aefb040cb0356c2db_rrj1ow.webp"
         alt="jediimg"
         className={styles.jedi}
       />
@@ -13,7 +13,7 @@ const Error = () => {
       <p className={styles.find}>
         Find this page, you will not. The force, you will need, but exist, it does not.
       </p>
-      <Button className="buttonname" />
+      <Button className={styles.buttonname} />
     </div>
   );
 };
