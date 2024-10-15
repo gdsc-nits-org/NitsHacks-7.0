4826d471b9c43ada981a5e0226cefa6dd0958a3c
rishinhk004 <rishirajsaha555@gmail.com>
2024-10-15 19:59:11 +0530

    analytics modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 import { Home } from "./Pages";
 import { Preview } from "./Components";
 
 const App = () => {
   const [loaded, setLoaded] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize("G-VVX3QJCVSM");
+    ReactGA.send("pageview", location.pathname + location.search);
+  }, [location]);
   return loaded ? <Home /> : <Preview setLoaded={setLoaded} />;
 };
 
