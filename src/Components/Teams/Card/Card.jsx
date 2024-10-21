0e641708210efb32f44888a726af3e3a31a49f21
barnilsarma <barnilsarma@gmail.com>
2024-10-21 18:53:41 +0530

    team page update

--- src/Components/Teams/Card/Card.jsx
+++ src/Components/Teams/Card/Card.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
+import { FaGlobe } from "react-icons/fa";
 import styles from "./Card.module.scss";
-
 const Card = (props) => {
   const [audio] = useState(new Audio("/sound/teamcard.wav"));
   const playm = () => {
@@ -16,61 +16,73 @@ const Card = (props) => {
         <div className={styles.box}>
           <img className={styles.img} src={props.img} alt="members" loading="lazy" />
         </div>
-        <div className={styles.content}>
+        <div
+          className={`${props.git || props.fb || props.linkedin || props.portfolio ? styles.content : styles.blankContent}`}
+        >
           <p className={styles.name}>{props.name}</p>
           {props.designation && <p className={styles.designation}>{props.designation}</p>}
         </div>
-        <div className={styles.glowbox}>
-          <div className={styles.social}>
-            {props.fb && (
-              <div className={styles.icon}>
-                <a href={props.fb} target="_blank" rel="noreferrer">
-                  <img
-                    className={`${styles.logo} ${styles.fbImg}`}
-                    src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1728845141/Vector_xj7xds.png"
-                    alt="facebook"
-                  />
-                </a>
-              </div>
-            )}
+        {(props.git || props.fb || props.linkedin || props.portfolio) && (
+          <div className={styles.glowbox}>
+            <div className={styles.social}>
+              {props.fb && (
+                <div className={styles.icon}>
+                  <a href={props.fb} target="_blank" rel="noreferrer">
+                    <img
+                      className={`${styles.logo} ${styles.fbImg}`}
+                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1728845141/Vector_xj7xds.png"
+                      alt="facebook"
+                    />
+                  </a>
+                </div>
+              )}
 
-            {props.linkedin && (
-              <div className={styles.icon}>
-                <a href={props.linkedin} target="_blank" rel="noreferrer">
-                  <img
-                    className={`${styles.logo} ${styles.linkedinImg}`}
-                    src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1728845141/ld_nrmndl.png"
-                    alt="linkedin"
-                  />
-                </a>
-              </div>
-            )}
+              {props.linkedin && (
+                <div className={styles.icon}>
+                  <a href={props.linkedin} target="_blank" rel="noreferrer">
+                    <img
+                      className={`${styles.logo} ${styles.linkedinImg}`}
+                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1728845141/ld_nrmndl.png"
+                      alt="linkedin"
+                    />
+                  </a>
+                </div>
+              )}
 
-            {props.git && (
-              <div className={styles.icon}>
-                <a href={props.git} target="_blank" rel="noreferrer">
-                  <img
-                    className={`${styles.logo} ${styles.gitImg}`}
-                    src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705429562/Nits%20hacks%206.0/Teams/lavkmin5prqostnlukqh.png"
-                    alt=" github"
-                  />
-                </a>
-              </div>
-            )}
+              {props.git && (
+                <div className={styles.icon}>
+                  <a href={props.git} target="_blank" rel="noreferrer">
+                    <img
+                      className={`${styles.logo} ${styles.gitImg}`}
+                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705429562/Nits%20hacks%206.0/Teams/lavkmin5prqostnlukqh.png"
+                      alt=" github"
+                    />
+                  </a>
+                </div>
+              )}
 
-            {props.cf && (
-              <div className={styles.icon}>
-                <a href={props.cf} target="_blank" rel="noreferrer">
-                  <img
-                    className={`${styles.logo} ${styles.cfImg}`}
-                    src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705486367/Nits%20hacks%206.0/Teams/lfqewnoiatuujzkelmqk.png"
-                    alt=" codeforces"
-                  />
-                </a>
-              </div>
-            )}
+              {props.cf && (
+                <div className={styles.icon}>
+                  <a href={props.cf} target="_blank" rel="noreferrer">
+                    <img
+                      className={`${styles.logo} ${styles.cfImg}`}
+                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705486367/Nits%20hacks%206.0/Teams/lfqewnoiatuujzkelmqk.png"
+                      alt=" codeforces"
+                    />
+                  </a>
+                </div>
+              )}
+
+              {props.portfolio && (
+                <div className={styles.icon}>
+                  <a href={props.portfolio} target="_blank" rel="noreferrer">
+                    <FaGlobe className={`${styles.logo} ${styles.portfolioImg}`} />
+                  </a>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
