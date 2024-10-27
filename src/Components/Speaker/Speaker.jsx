370b507fa5efdb953269fc55176eaa188d4e8631
A-nirvana <anirban_ug_23@gmail.com>
2024-10-27 08:27:49 +0530

    Speaker Added

---
+++ src/Components/Speaker/Speaker.jsx
@@ -0,0 +1,60 @@
+import styles from "./Speaker.module.scss";
+
+const Card = ({ image, name, role, data }) => {
+  return (
+    <div className={styles.flip_card}>
+      <div className={styles.flip_card_front}>
+        <div className={styles.flip_card_inner}>
+          <img src={image} className={styles.speaker_image} alt={name} />
+          <h1 className={styles.name}>{name}</h1>
+          <h2 className={styles.role}>{role}</h2>
+        </div>
+      </div>
+      <div className={styles.flip_card_back}>
+        <div className={styles.flip_card_inner}>
+          <ul className={styles.data}>
+            {data.map((info) => (
+              <li key={image}>{info}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Speaker = () => {
+  const speakers = [
+    {
+      id: 1,
+      image:
+        "https://res.cloudinary.com/dmezugavw/image/upload/v1729997061/kamlesh_epoa9x.jpg",
+      name: "Kamlesh Nagware",
+      role: "Co-Founder @ FSV Capital",
+      data: "TEDx Speaker|Blockchain TOP VOICE | Co-chair of the IEEE Blockchain Initiative|Mentor to Blockchain start-ups|Former Blockchain Consultant at IBM",
+    },
+  ];
+  return (
+    <section className={styles.section} id="speakers">
+      <h1 className={styles.heading}>our speakers</h1>
+      <div className={styles.SWjudge}>
+        <div className={styles.gridContainer}>
+          {speakers.map((speaker) => {
+            const dataList = speaker.data.split("|");
+            return (
+              <Card
+                key={speaker.id}
+                image={speaker.image}
+                name={speaker.name}
+                role={speaker.role}
+                data={dataList}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Speaker;
