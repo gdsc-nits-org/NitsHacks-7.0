84f1840670d7e0d95eb210b25626094520f7ceaa
RISHI RAJ SAHA <132642857+rishinhk004@users.noreply.github.com>
2024-11-02 20:05:00 +0530

    Update Carausel.jsx

--- src/Components/Carausel/Carausel.jsx
+++ src/Components/Carausel/Carausel.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./Carausel.module.scss";
 
 const MAX_VISIBILITY = 3;
-const AUTOPLAY_INTERVAL = 3000;
+//const AUTOPLAY_INTERVAL = 3000;
 
 const Carousel = ({ children }) => {
   const [active, setActive] = useState(0);
