8bd6cb19fcb9e858c6ab3ab6a3c02b854ab44e50
barnilsarma <barnilsarma@gmail.com>
2024-11-10 13:02:27 +0530

    final standings

--- src/Pages/CFStandings/CfStandings.jsx
+++ src/Pages/CFStandings/CfStandings.jsx
@@ -12,7 +12,7 @@ async function pullData(message, time) {
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
   const res = await fetch(
-    `https://codeforces.com/api/contest.standings?contestId=563188&apiKey=${key}&time=${time}&apiSig=123456<${hashHex}>`
+    `https://codeforces.com/api/contest.standings?contestId=564294&apiKey=${key}&time=${time}&apiSig=123456<${hashHex}>`
   );
   if (!res.ok) throw new Error("Network response was not ok");
   return res.json();
@@ -42,7 +42,7 @@ const CfStandings = () => {
 
 const Wrapper = () => {
   const nowTime = Math.floor(Date.now() / 1000);
-  const code = `123456/contest.standings?apiKey=${key}&contestId=563188&time=${nowTime}#${secret}`;
+  const code = `123456/contest.standings?apiKey=${key}&contestId=564294&time=${nowTime}#${secret}`;
   const { isLoading, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () => pullData(code, nowTime),
@@ -76,7 +76,7 @@ const Wrapper = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.headerCont}>
-        <h1 className={styles.header}>prelims standings</h1>
+        <h1 className={styles.header}>finals standings</h1>
       </div>
       <div className={styles.inputCont}>
         <input
