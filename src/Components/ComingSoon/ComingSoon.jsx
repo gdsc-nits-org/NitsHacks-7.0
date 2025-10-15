58aa4b35e557763ed7b630b67832cfdf20e1cbe2
hirakRajbonshi <hrkraj0@gmail.com>
2025-10-16 01:56:30 +0530

    fix

--- src/Components/ComingSoon/ComingSoon.jsx
+++ src/Components/ComingSoon/ComingSoon.jsx
@@ -1,15 +1,7 @@
-import React from "react";
-import { useEffect } from "react";
-import { gsap } from "gsap";
-import { SteppedEase } from "gsap";
-import "./ComingSoon.css";
+import React, { useEffect } from "react";
+import { gsap, SteppedEase } from "gsap";
+import "./ComingSoonStyle.css";
 import sponsors from "../../assets/sponsors.json";
-import runPikachu from "/images/run-pikachu.gif";
-import bikingGirl from "/images/bikingGirl.gif";
-import logo from "/images/logo.png";
-import cloudMov from "/images/cloudMov.png";
-import clouds1 from "/images/clouds1.png";
-import grass from "/images/grass.png";
 
 gsap.registerPlugin(SteppedEase);
 
@@ -29,45 +21,60 @@ const ComingSoon = () => {
       repeat: -1,
     });
   }, []);
+
   return (
     <section id="coming-soon">
       <div className="blueBg">
         <div
           className="background-layer clouds-bg"
-          style={{ backgroundImage: `url(${cloudMov})` }}
+          style={{ backgroundImage: "url('/images/cloudMov.png')" }}
         ></div>
         <div
           className="background-layer ground"
-          style={{ backgroundImage: `url(${grass})` }}
+          style={{ backgroundImage: "url('/images/grass.png')" }}
         ></div>
+
         <div className="comingContainer">
-          <img src={clouds1} className="clouds1" alt="clouds" />
+          <img src="/images/clouds1.png" className="clouds1" alt="clouds" />
           <h1
             className="coming-soon-text"
             style={{ fontFamily: "PokemonFont, Arial, sans-serif" }}
           >
             COMING SOON
           </h1>
-          <img src={clouds1} className="clouds1" alt="clouds" />
+          <img src="/images/clouds1.png" className="clouds1" alt="clouds" />
         </div>
+
         <div className="center-content">
-          <img src={logo} className="logo" alt="logo" />
+          <img src="/images/logo.png" className="logo" alt="logo" />
           <div className="character-container">
-            <img className="character-pikachu" src={runPikachu} alt="Pikachu" />
-            <img className="character-bike" src={bikingGirl} alt="Character on bike" />
+            <img
+              className="character-pikachu"
+              src="/images/run-pikachu.gif"
+              alt="Pikachu"
+            />
+            <img
+              className="character-bike"
+              src="/images/bikingGirl.gif"
+              alt="Character on bike"
+            />
           </div>
         </div>
-        <img src={grass} className="staticGrass" alt="grass" />
+
+        <img src="/images/grass.png" className="staticGrass" alt="grass" />
+
         <div className="grad-1"></div>
         <div className="grad-2"></div>
         <div className="grad-3"></div>
         <div className="grad-4"></div>
         <div className="grad-5"></div>
       </div>
+
       <div style={{ height: "1rem", width: "100vw", backgroundColor: "#2E7915" }}></div>
       <div style={{ height: "1rem", width: "100vw", backgroundColor: "#339900" }}></div>
       <div style={{ height: "1rem", width: "100vw", backgroundColor: "#BBDC00" }}></div>
       <div style={{ height: "1rem", width: "100vw", backgroundColor: "#F0E800" }}></div>
+
       <div className="footer">
         <section className="sponsor-section">
           <h2
@@ -78,16 +85,18 @@ const ComingSoon = () => {
           </h2>
           <div className="carousel">
             <div className="carousel-track">
-              {[...sponsors, ...sponsors].map((sponsor, index) => (
+              {" "}
+              {[...sponsors, ...sponsors].map((sponsor) => (
                 <a
-                  key={index}
+                  key={sponsor.id}
                   href={sponsor.uri}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={sponsor.img} alt={sponsor.name} />
+                  {" "}
+                  <img src={sponsor.img} alt={sponsor.name} />{" "}
                 </a>
-              ))}
+              ))}{" "}
             </div>
           </div>
         </section>
